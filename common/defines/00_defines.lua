ad7cc6a3dd7e9869e24cd76096c1f635da4954d9
NeoXenocide <morgan.shepard@gmail.com>
2019-05-08 01:16:08 -0400

    tweaked fuel conversion very slightly. Nerfed CSA and USA's oil production a bit. Added Argintine fleet fix.

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -298,7 +298,7 @@ NCountry = {
 	
 	STARTING_FUEL_RATIO = 0.25,						-- starting fuel ratio compared to max fuel for countries
 	BASE_FUEL_GAIN_PER_OIL = 2,						-- base amount of fuel gained hourly per excess oil
-	BASE_FUEL_GAIN = 2.0,							-- base amount of fuel gained hourly, independent of excess oil
+	BASE_FUEL_GAIN = 3.0,							-- base amount of fuel gained hourly, independent of excess oil
 	BASE_FUEL_CAPACITY = 50000,						-- base amount of fuel capacity
 
 	COUNTRY_MANPOWER_CAPITULATED_FREE_POOL_FACTOR = 0.1,	-- Factor on amount of normal manpower left for an exiled nation with no territory.
